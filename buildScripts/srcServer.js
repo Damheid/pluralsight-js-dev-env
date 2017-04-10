766d7cb70d355268a6edad7735238b75cc7fd994
Adam Heiderich <Adam@Adams-MacBook-Pro.local>
2017-04-10 19:22:38 +0200

    Latest changes

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -16,14 +16,22 @@ app.use(require('webpack-dev-middleware')(compiler, {
 }));
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {
-	if(err) {
-		console.log(err);
-	}
-	else {
-		open('http://localhost:' + port);
-	}
+app.get('/users', function (req, res) {
+  res.json([
+    { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
+    { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@gmail.com" },
+    { "id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@gmail.com" }
+  ]);
+});
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    open('http://localhost:' + port);
+  }
 });
